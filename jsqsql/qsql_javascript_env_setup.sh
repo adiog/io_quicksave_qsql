22eab3723a5f1ca0288d0056c1a557d4a7daa2e7
Aleksander Gajewski <adiog@brainfuck.pl>
2017-10-13 06:16:50 +0200

    fixing travis

--- jsqsql/qsql_javascript_env_setup.sh
+++ jsqsql/qsql_javascript_env_setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-. ${WORKSPACE}/env.sh
-
-
 function install_node_and_deps_on_debian()
 {
-    sudo apt-get install nodejs nodejs-legacy npm
+    sudo apt-get install nodejs nodejs-legacy npm -y
     sudo npm install antlr4 --global
     sudo npm install browserify --global
     sudo npm install node-minify uglify-js --global
