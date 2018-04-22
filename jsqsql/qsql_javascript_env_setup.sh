a2bd9a0ed7646b9c4cb74c233ac01e7f60c3c33a
adiog <adiog@brainfuck.pl>
2018-04-22 07:56:10 +0200

    ignore ssl

--- jsqsql/qsql_javascript_env_setup.sh
+++ jsqsql/qsql_javascript_env_setup.sh
@@ -3,6 +3,7 @@
 function install_node_and_deps_on_debian()
 {
     sudo apt-get install nodejs nodejs-legacy npm -y
+    sudo npm config set strict-ssl false
     sudo npm install antlr4 --global
     sudo npm install browserify --global
     sudo npm install node-minify uglify-js --global
