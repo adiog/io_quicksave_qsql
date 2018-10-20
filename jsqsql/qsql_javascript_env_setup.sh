565eddd9628340ef418ceaf67794feafa2cea5b5
Aleksander Gajewski <adiog@brainfuck.pl>
2018-10-20 16:29:58 +0200

    fix travis

--- jsqsql/qsql_javascript_env_setup.sh
+++ jsqsql/qsql_javascript_env_setup.sh
@@ -2,6 +2,8 @@
 
 function install_node_and_deps_on_debian()
 {
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get install -y nodejs
     sudo apt-get install nodejs nodejs-legacy npm -y
     sudo npm config set strict-ssl false
     sudo npm install antlr4 --global
